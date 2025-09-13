731451f8f8a8c28b89f089708901c2603d3415f3
shengshampoo <shengzh@tutanota.com>
2025-09-13 15:49:32 +0800

    Create build-static-gocryptfs.sh

---
+++ build-static-gocryptfs.sh
@@ -0,0 +1,29 @@
+
+#! /bin/bash
+
+set -e
+
+WORKSPACE=/tmp/workspace
+mkdir -p $WORKSPACE
+mkdir -p /work/artifact
+
+# libressl
+cd $WORKSPACE
+aa=4.1.0
+curl -sL https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-$aa.tar.gz | tar xv --gzip
+cd libressl-$aa
+./configure --prefix=/usr --disable-tests -disable-shared --enable-static
+make
+make install
+
+# gocryptfs
+cd $WORKSPACE
+git clone https://github.com/rfjakob/gocryptfs.git
+cd gocryptfs
+cat /gocryptfs.static.patch | patch -p0
+GOAMD64=v3 GOOS=linux GOARCH=amd64 CGO_ENABLED=1 bash ./build.bash
+
+cd $WORKSPACE/gocryptfs
+tar vcJf ./gocryptfs.tar.xz gocryptfs
+
+mv ./gocryptfs.tar.xz /work/artifact/

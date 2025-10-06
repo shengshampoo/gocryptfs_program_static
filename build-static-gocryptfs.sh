93ce17d89392c220cf0d05cda0fb954bb1714009
shengshampoo <shengzh@tutanota.com>
2025-10-06 12:57:41 +0800

    Update build-static-gocryptfs.sh

--- build-static-gocryptfs.sh
+++ build-static-gocryptfs.sh
@@ -10,7 +10,7 @@ mkdir -p /work/artifact
 # libressl
 cd $WORKSPACE
 aa=4.1.1
-curl -sL https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-$aa.tar.gz | tar xv --gzip
+curl -sL https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-$aa.tar.gz | tar x --gzip
 cd libressl-$aa
 ./configure --prefix=/usr --disable-tests -disable-shared --enable-static
 make
@@ -33,3 +33,7 @@ cd $WORKSPACE/gocryptfs
 tar vcJf ./gocryptfs.tar.xz gocryptfs
 
 mv ./gocryptfs.tar.xz /work/artifact/
+
+cd /usr/bin
+tar vcJf ./openssl.tar.xz openssl
+mv ./openssl.tar.xz /work/artifact/
